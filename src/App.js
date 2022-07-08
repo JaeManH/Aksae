4856d56215d9c2540b806c779ac38bc65ac2b73d
JaeMan <boool213@naver.com>
2022-07-08 13:24:48 +0900

    기본 화면 틀 제작

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <header>
+        <a href='{App}'><img src={logo} className='Logo_Img'></img></a>
+        <div>
+          <a href='#'><div className='Header_items'>about us</div></a>
+          <a href='#'><div className='Header_items'>서비스</div></a>
+          <a href='#'><div className='Header_items'>설립개요</div></a>
+          <a href='#'><div className='Header_items'>모티브</div></a>
+          <a href='#'><div className='Header_items'>기대효과</div></a>
+          <a href='#'><div className='Header_items'>미식등급</div></a>
+        </div>
       </header>
+      <div className='Content_container'>
+        <h1>컨텐츠 들어가는곳</h1>
+      </div>
+      <footer>
+       <h1>ⓒ 2022. Aksae all rights reserved.</h1>
+      </footer>
     </div>
   );
 }
